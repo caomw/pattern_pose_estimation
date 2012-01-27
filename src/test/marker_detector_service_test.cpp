a4fd2dd93014094e87d6c256a0f4a40aab78ffe7
Stephan Wirth <stephan.wirth@uib.es>
2012-01-27 13:20:46 +0100

    renamed files, better parameterized test

--- src/test/marker_detector_service_test.cpp
+++ src/test/marker_detector_service_test.cpp
@@ -9,10 +9,36 @@
 #include "marker_detector_server.h"
 #include "pattern_pose_estimation/DetectMarker.h"
 
+struct TestData
+{
+  std::string image_file;
+  std::string camera_info_file;
+  bool rectified;
+  int expected_num_markers;
+
+  TestData(const std::string& img_file, const std::string& caminfo_file,
+      bool rect, int num_markers) :
+    image_file(img_file),
+    camera_info_file(caminfo_file),
+    rectified(rect),
+    expected_num_markers(num_markers)
+  {}
+};
+
+std::vector<TestData> getDataSets()
+{
+  std::vector<TestData> data_sets;
+  data_sets.push_back(TestData("markers.jpg", "ueye.yaml", true, 8));
+  data_sets.push_back(TestData("no_markers.jpg", "ueye.yaml", true, 0));
+  return data_sets;
+}
+
 sensor_msgs::Image loadImage(const std::string& filename)
 {
-  cv::Mat image = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
-  if(image.empty()) throw std::runtime_error("image load failed");
+  std::string pkg_path = ros::package::getPath(ROS_PACKAGE_NAME);
+  std::string data_path = pkg_path + "/test/data/";
+  cv::Mat image = cv::imread(data_path + filename, CV_LOAD_IMAGE_COLOR);
+  if(image.empty()) throw std::runtime_error("image load failed: " + data_path + filename);
   sensor_msgs::Image image_msg;
   cv_bridge::CvImage cv_image;
   cv_image.image = image;
@@ -23,50 +49,49 @@ sensor_msgs::Image loadImage(const std::string& filename)
 
 sensor_msgs::CameraInfo loadCameraInfo(const std::string& filename)
 {
+  std::string pkg_path = ros::package::getPath(ROS_PACKAGE_NAME);
+  std::string data_path = pkg_path + "/test/data/";
   std::string camera_name;
   sensor_msgs::CameraInfo camera_info;
-  bool ok = camera_calibration_parsers::readCalibration(filename, camera_name, camera_info);
+  bool ok = camera_calibration_parsers::readCalibration(data_path + filename, camera_name, camera_info);
   if (!ok) throw std::runtime_error("camera info load failed");
   return camera_info;
 }
 
-TEST(MarkerDetectorServiceTest, detectionTest)
+class MarkerDetectorServiceTest : public ::testing::TestWithParam<TestData>
+{};
+
+
+TEST_P(MarkerDetectorServiceTest, detectionTest)
 {
-  // load data
-  std::string pkg_path = ros::package::getPath(ROS_PACKAGE_NAME);
-
-  sensor_msgs::Image image_with_marker_msg = loadImage(pkg_path + "/test/data/marker.jpg");
-  sensor_msgs::Image image_without_marker_msg = loadImage(pkg_path + "/test/data/no_marker.jpg");
-
-  sensor_msgs::CameraInfo camera_info = loadCameraInfo(pkg_path + "/test/data/camera_info.yaml");
+  sensor_msgs::Image image_msg = loadImage(GetParam().image_file);
+  sensor_msgs::CameraInfo camera_info = loadCameraInfo(GetParam().camera_info_file);
 
   ros::NodeHandle nh;
   ros::ServiceClient client = 
-    nh.serviceClient<pattern_pose_estimation::DetectMarker>("marker_detector_server/detect_marker");
+    nh.serviceClient<pattern_pose_estimation::DetectMarker>(
+        "marker_detector_server/detect_marker");
   bool service_found = client.waitForExistence(ros::Duration(5));
   ASSERT_TRUE(service_found);
 
   pattern_pose_estimation::DetectMarker service_msg;
 
-  service_msg.request.image = image_with_marker_msg;
+  service_msg.request.image = image_msg;
   service_msg.request.camera_info = camera_info;
-  service_msg.request.rectified = true;
+  service_msg.request.rectified = GetParam().rectified;
 
   EXPECT_TRUE(client.call(service_msg));
 
-  EXPECT_EQ(service_msg.response.markers.markers.size(), 8);
+  std::cout << "Detected " << service_msg.response.markers.markers.size()
+    << " markers in " << GetParam().image_file << std::endl;
 
-  pattern_pose_estimation::DetectMarker service_msg_2;
-
-  service_msg.request.image = image_without_marker_msg;
-  service_msg.request.camera_info = camera_info;
-  service_msg.request.rectified = true;
-
-  EXPECT_TRUE(client.call(service_msg));
-
-  EXPECT_EQ(service_msg.response.markers.markers.size(), 0);
+  EXPECT_EQ(service_msg.response.markers.markers.size(), 
+      GetParam().expected_num_markers);
 }
   
+INSTANTIATE_TEST_CASE_P(MarkerDetectorServiceTests,
+    MarkerDetectorServiceTest,
+    ::testing::ValuesIn(getDataSets()));
 int main(int argc, char **argv)
 {
   testing::InitGoogleTest(&argc, argv);
